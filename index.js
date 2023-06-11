2aabb8754da08c2aebc4f3e555f963d49c35c001
juliannecc <julianneclave@gmail.com>
2023-06-11 17:16:50 +0800

    test algorithm

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const added_files = core.getInput('added_files');
+const levenshtein = require('fast-levenshtein'); 
+
+const levenshteinFilter = (source, maximum = 5) => {
+  let _source, matches, x, y;
+  _source = source.slice();
+  matches = [];
+  for (x = _source.length - 1; x >= 0; x--) {
+    let output = _source.splice(x, 1);
+    for (y = _source.length - 1; y >= 0; y--) {
+      if (levenshtein.get(output[0], _source[y]) <= maximum) {
+        output.push(_source[y]);
+        _source.splice(y, 1);
+        x--;
+      }
+    }
+    matches.push(output);
+  }
+  return matches;
+}
+
 
 function strToArr (str){
     return str.split(' ');
@@ -12,6 +32,11 @@ function strToArr (str){
         try {
             core.notice("Sample");
             core.info(strToArr(added_files));
+
+            let source = ['06-05-23/Control/1.png', '06-05-23/Control/2.png', '06-05-23/-K/1.png', '06-05-23/-K/2.png', '06-05-23/-N/1.png', '06-06-23/-N/1.png'];
+            let output = levenshteinFilter(source);
+            core.info(output);
+
         } catch (error) {
             core.setFailed(error.message);
         }
