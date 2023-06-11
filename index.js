ec6b07c712a9b51fb00165d5916c7d4a587262a0
juliannecc <julianneclave@gmail.com>
2023-06-11 22:39:25 +0800

    debug

--- index.js
+++ index.js
@@ -67,14 +67,13 @@ client.authorize(function(err, tokens){
         const fileObj  = groupArr(strToArr(added_files));
         let fileList = Object.keys(fileObj);
         fileList.forEach((folder) => {
-            let sheetTemp = folder;
             let sheet = folder.split("/")[1];
               let col = numberToExcelHeader(parseInt(folder.split("/")[0].split("-")[1]));
               fileObj[folder].forEach((file) => {
               let row = parseInt(file.match(/(?<=_)[0-9]*/gi)[0])+2
               let cell = col+row;
-              let link = `https://raw.githubusercontent.com/${owner}/${repo}/main/${file}`;
-              console.log(sheet,sheetTemp, cell, link)
+              let link = `=Image("https://raw.githubusercontent.com/${owner}/${repo}/main/${file}")`;
+              console.log(sheet, cell, link)
               gsrun(client, sheet, cell, [[link]]);
             });
         });
