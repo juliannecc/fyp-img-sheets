d35356a69423ebe0436d9d0169424c3fb0ff64ea
juliannecc <julianneclave@gmail.com>
2023-06-11 18:14:49 +0800

    add group arrfunction

--- index.js
+++ index.js
@@ -2,42 +2,30 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const added_files = core.getInput('added_files');
-const levenshtein = require('fast-levenshtein'); 
-
-const levenshteinFilter = (source, maximum = 3) => {
-  let _source, matches, x, y;
-  _source = source.slice();
-  matches = [];
-  for (x = _source.length - 1; x >= 0; x--) {
-    let output = _source.splice(x, 1);
-    for (y = _source.length - 1; y >= 0; y--) {
-      if (levenshtein.get(output[0], _source[y]) <= maximum) {
-        output.push(_source[y]);
-        _source.splice(y, 1);
-        x--;
-      }
-    }
-    matches.push(output);
-  }
-  return matches;
-}
-
 
 function strToArr (str){
     return str.split(' ');
 }
+
+function groupArr (arr){
+    const binned = arr.reduce((result, word) => {
+        const letter = word.substring(0,12);
+        result[letter] = result[letter] || [];
+        result[letter].push(word);
+        return result;
+      }, {})
+
+      return binned;
+}
+
 
 (
     async () => {
         try {
             core.notice("Sample");
             core.info(strToArr(added_files));
-
             let source = ['06-05-23/Control/1.png', '06-05-23/Control/2.png', '06-05-23/-K/1.png', '06-05-23/-K/2.png', '06-05-23/-N/1.png', '06-06-23/-N/1.png'];
-            let output = levenshteinFilter(source);
-            core.info(output);
-            console.log(JSON.stringify(output));
-
+            core.info(groupArr(source));
         } catch (error) {
             core.setFailed(error.message);
         }
