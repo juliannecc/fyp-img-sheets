75f85fd3d278694ce33c58f324a7a6676e8c9553
juliannecc <julianneclave@gmail.com>
2023-06-11 15:53:59 +0800

    add string to arr function

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const github = require('@actions/github');
 
 const added_files = core.getInput('added_files');
 
+function strToArr (str){
+    return str.split(' ');
+}
+
 (
     async () => {
         try {
             core.notice("Sample");
-            core.info(added_files);
+            core.info(strToArr(added_files));
         } catch (error) {
             core.setFailed(error.message);
         }
