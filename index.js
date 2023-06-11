2a8b014dccba95061017eccf525fda202c0ed3c8
juliannecc <julianneclave@gmail.com>
2023-06-11 13:38:38 +0800

    add getCommit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const token = core.getInput('token');
+const octokit = github.getOctokit(token);
+
+const repo = core.getInput('repo');
+const commit_id = core.getInput('commit_id');
+
+async function getCommit(owner, repo, commit_id){
+    try {
+        const commitOut = octokit.rest.git.getCommit({
+            owner: `${owner}`,
+            repo: `${repo}`,
+            commit_sha: `${commit_id}`,
+          });
+
+        return commitOut;
+    } catch (error) {
+        core.setFailed(error)
+    }
+
+}
+
 (
     async () => {
         try {
-            core.notice("Sample")
+            core.notice("Sample");
+            core.info(getCommit(owner, repo, commit_id));
         } catch (error) {
             core.setFailed(error.message);
         }
