7d1e82f8e69d758ae25502fbe8029bafb081d9cf
juliannecc <julianneclave@gmail.com>
2023-06-11 15:29:30 +0800

    edit index

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const token = core.getInput('token');
-const octokit = github.getOctokit(token);
-
-const owner = core.getInput('owner');
-const repo = core.getInput('repo');
-const commit_id = core.getInput('commit_id');
-
-async function getCommit(owner, repo, commit_id){
-    try {
-        const commitOut = octokit.rest.git.getCommit({
-            owner: `${owner}`,
-            repo: `${repo}`,
-            commit_sha: `${commit_id}`,
-          });
-
-        return commitOut;
-    } catch (error) {
-        core.setFailed(error)
-    }
-
-}
+const added_files = core.getInput('added_files');
 
 (
     async () => {
         try {
             core.notice("Sample");
-            const commitOutput = getCommit(owner, repo, commit_id);
-            commitOutput.then((response) => {
-                core.info(JSON.stringify(response.data));
-            })
+            core.info(added_files);
         } catch (error) {
             core.setFailed(error.message);
         }
